35ff6e1fea053d1e0857f0b1d3ffd45c75aca52c
Ha3 <git@ha3.eu>
2018-07-18 20:49:37 +0200

    Initial commit

---
+++ src/main/kotlin/restx/RestService.kt
@@ -0,0 +1,33 @@
+package restx
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+open class GreetingController {
+    val counter = 0L
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter, "Hello, $name")
+
+    @PostMapping("/payloads")
+    fun uploadPayload(@RequestParam("file") file: MultipartFile): ResponseEntity<Long> =
+            ResponseEntity.status(201).body(file.size)
+
+}
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
---
+++ src/test/kotlin/restx/GreetingControllerTest.kt
@@ -0,0 +1,67 @@
+package restx
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.mock.web.MockMultipartFile
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
+import org.springframework.web.context.WebApplicationContext
+import java.nio.charset.StandardCharsets
+
+
+/**
+ * (Default template)
+ * Created on 2018-07-18
+ *
+ * @author Ha3
+ */
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [Application::class])
+public class GreetingControllerTest {
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    private lateinit var mockMvc: MockMvc;
+
+    @Before
+    public fun setup() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public fun `it should return a greeting`() {
+        mockMvc.perform(get("/greeting"))
+                .andExpect(status().isOk)
+                .andExpect(content().string("""{"id":0,"content":"Hello, World"}"""))
+    }
+
+    @Test
+    public fun `it should return a custom greeting`() {
+        mockMvc.perform(
+                get("/greeting?name=Gérald")
+        )
+                .andExpect(status().isOk)
+                .andExpect(content().string("""{"id":0,"content":"Hello, Gérald"}"""))
+    }
+
+    @Test
+    public fun `it should upload a payload and return its size`() {
+        mockMvc.perform(fileUpload("/payloads").file(MockMultipartFile("file", "hello".toByteArray(StandardCharsets.UTF_8))))
+                .andExpect(status().isCreated)
+                .andExpect(content().string("""5"""))
+    }
+
+    @Test
+    public fun `it should reject a payload`() {
+        mockMvc.perform(fileUpload("/payloads"))
+                .andExpect(status().isBadRequest)
+    }
+}
