742378695fb947ee6a8c41ff16c11998670b74e7
gyam <git@ha3.eu>
2018-10-12 09:00:30 +0200

    (feat) Add additional date serialization test

--- connector-vertx-common/src/test/kotlin/eu/ha3/x/sff/connector/vertx/JsonifyTest.kt
+++ connector-vertx-common/src/test/kotlin/eu/ha3/x/sff/connector/vertx/JsonifyTest.kt
@@ -4,6 +4,7 @@ import io.vertx.core.json.JsonArray
 import io.vertx.kotlin.core.json.JsonObject
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
@@ -46,9 +47,44 @@ internal class JsonifyTest {
         assertThat(result).isEqualTo(expected)
     }
 
+    @Test
+    internal fun `it should convert non-utc model to json`() {
+        val model = JsonifyTest.MODEL_ZONED_PARIS
+
+        // Exercise
+        val result = model.jsonify()
+
+        // Verify
+        result.inner.apply {
+            assertThat(getString("someZdt")).isEqualTo("2010-12-30T22:00:04.123+01:00[Europe/Paris]")
+            assertThat(getJsonArray("someList")).containsExactly("a", "b")
+            assertThat(getString("someString")).isEqualTo("hello")
+        }
+    }
+
+    @Test
+    internal fun `it should convert json to non-utc model`() {
+        val expected = JsonifyTest.MODEL_ZONED_PARIS
+        val json = DJsonObject(JsonObject()
+                .put("someZdt", "2010-12-30T22:00:04.123+01:00[Europe/Paris]")
+                .put("someList", JsonArray(listOf("a", "b")))
+                .put("someString", "hello"))
+
+        // Exercise
+        val result = json.dejsonify(ExampleModel::class.java)
+
+        // Verify
+        assertThat(result).isEqualTo(expected)
+    }
+
     companion object {
         val MODEL = ExampleModel(
                 ZonedDateTime.of(2010, 12, 30, 22, 0, 4, 123_000_000, ZoneOffset.UTC),
+                listOf("a", "b"),
+                "hello"
+        )
+        val MODEL_ZONED_PARIS = ExampleModel(
+                ZonedDateTime.of(2010, 12, 30, 22, 0, 4, 123_000_000, ZoneId.of("Europe/Paris")),
                 listOf("a", "b"),
                 "hello"
         )
