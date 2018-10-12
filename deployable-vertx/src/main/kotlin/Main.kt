787d594cf7d0c74f62f1f27d4eb8313c1ce25c04
gyam <git@ha3.eu>
2018-10-12 19:37:42 +0200

    (fix) Add missing codec in main classes

--- deployable-vertx/src/main/kotlin/Main.kt
+++ deployable-vertx/src/main/kotlin/Main.kt
@@ -1,9 +1,6 @@
 
 import eu.ha3.x.sff.api.DocStorage
-import eu.ha3.x.sff.connector.vertx.DocStorageVerticle
-import eu.ha3.x.sff.connector.vertx.DocSystemVerticle
-import eu.ha3.x.sff.connector.vertx.VersDocSystem
-import eu.ha3.x.sff.connector.vertx.WebVerticle
+import eu.ha3.x.sff.connector.vertx.*
 import eu.ha3.x.sff.core.Doc
 import eu.ha3.x.sff.system.IDocSystem
 import io.reactivex.Single
@@ -18,6 +15,8 @@ import java.time.ZonedDateTime
  */
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
+    vertx.eventBus().registerDefaultCodec(DJsonObject::class.java, DJsonObjectMessageCodec())
+
     val verticles = listOf(
             WebVerticle(),
             DocStorageVerticle(DocStorage(VersDocSystem())),
@@ -25,7 +24,5 @@ fun main(args: Array<String>) {
                 override fun listAll(): Single<List<Doc>> = Single.just(listOf(Doc("hello", ZonedDateTime.now())))
             })
     )
-    verticles.forEach {
-        vertx.deployVerticle(it)
-    }
+    verticles.forEach(vertx::deployVerticle)
 }
--- deployable-vertx/src/main/kotlin/MainNoEventBus.kt
+++ deployable-vertx/src/main/kotlin/MainNoEventBus.kt
@@ -1,5 +1,7 @@
 
 import eu.ha3.x.sff.api.DocStorage
+import eu.ha3.x.sff.connector.vertx.DJsonObject
+import eu.ha3.x.sff.connector.vertx.DJsonObjectMessageCodec
 import eu.ha3.x.sff.connector.vertx.DocStorageVerticle
 import eu.ha3.x.sff.connector.vertx.WebVerticle
 import eu.ha3.x.sff.core.Doc
@@ -16,13 +18,13 @@ import java.time.ZonedDateTime
  */
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
+    vertx.eventBus().registerDefaultCodec(DJsonObject::class.java, DJsonObjectMessageCodec())
+
     val verticles = listOf(
             WebVerticle(),
             DocStorageVerticle(DocStorage(object : IDocSystem {
                 override fun listAll(): Single<List<Doc>> = Single.just(listOf(Doc("hello", ZonedDateTime.now())))
             }))
     )
-    verticles.forEach {
-        vertx.deployVerticle(it)
-    }
+    verticles.forEach(vertx::deployVerticle)
 }
