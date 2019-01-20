4fc9e10aed84c38808380b7864112104c173882a
Ha3 <git@ha3.eu>
2019-01-20 16:10:29 +0100

    (evolve) type NoMessage is now an alias for Unit

--- core/src/main/kotlin/eu/ha3/x/sff/core/DocModels.kt
+++ core/src/main/kotlin/eu/ha3/x/sff/core/DocModels.kt
@@ -13,4 +13,4 @@ data class Doc(val name: String, val createdAt: ZonedDateTime)
 data class DocCreateRequest(val name: String)
 data class DocListResponse(val data: List<Doc>)
 
-object NoMessage
+typealias NoMessage = Unit
