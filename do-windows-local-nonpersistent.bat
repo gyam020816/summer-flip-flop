a8682559448319afc5c9744c1eaa3459dca84e4a
Ha3 <git@ha3.eu>
2019-01-21 22:17:19 +0100

    (evolve) use alternate way to set variables

--- do-windows-local-nonpersistent.bat
+++ do-windows-local-nonpersistent.bat
@@ -3,4 +3,4 @@ REM warning: if you write "set DB_USER=postgres && set DB_PASSW..."
 REM   then DB_USER will be equal to "postgres " with a trailing space (9 chars)
 REM   :(   https://youtu.be/3YxaaGgTQYM?t=11
 
-set DB_NAME=summer&& set DB_USER=postgres&& set DB_PASSWORD=test123&& set DB_URL=jdbc:postgresql://localhost:16099/summer&& docker-compose up -d && mvn install -DskipTests -f ./pom.xml && mvn exec:java -DskipTests -Dexec.mainClass="eu.ha3.x.sff.deployable.SMainNoEventBusPostgresKt" -f ./deployable-vertx/pom.xml
+set "DB_NAME=summer" && set "DB_USER=postgres" && set "DB_PASSWORD=test123" && set "DB_URL=jdbc:postgresql://localhost:16099/summer" && docker-compose up -d && mvn install -DskipTests -f ./pom.xml && mvn exec:java -DskipTests -Dexec.mainClass="eu.ha3.x.sff.deployable.SMainNoEventBusPostgresKt" -f ./deployable-vertx/pom.xml
