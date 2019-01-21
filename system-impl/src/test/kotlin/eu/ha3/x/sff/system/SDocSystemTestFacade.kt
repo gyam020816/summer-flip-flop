6920286429d96b1e1f6e63950c3c8a5c58da82b3
Ha3 <git@ha3.eu>
2019-01-21 23:01:02 +0100

    (other) add a test to make sure time zones are preserved in Postgres jsonb

--- system-impl/src/test/kotlin/eu/ha3/x/sff/system/SDocSystemTestFacade.kt
+++ system-impl/src/test/kotlin/eu/ha3/x/sff/system/SDocSystemTestFacade.kt
@@ -39,6 +39,18 @@ interface SDocSystemTestFacade<T : SDocSystem> {
     }
 
     @Test
+    fun `it should append to docs and return it, preserving the time zone (facade)`() = testBlocking {
+        val item = Doc("a", ZonedDateTime.of(2000, 12, 1, 23, 40, 50, 0, ZoneOffset.of("+07:15")))
+
+        // Exercise
+        SUT().appendToDocs(item)
+        val result = SUT().listAll()
+
+        // Verify
+        assertThat(result).isEqualTo(DocListResponse(listOf(item)))
+    }
+
+    @Test
     fun `it should append to docs and return them by createdAt property (facade)`() = testBlocking {
         val item2001 = Doc("a", ZonedDateTime.of(2001, 12, 1, 23, 40, 50, 0, ZoneOffset.UTC))
         val item1999 = Doc("a", ZonedDateTime.of(1999, 12, 1, 23, 40, 50, 0, ZoneOffset.UTC))
