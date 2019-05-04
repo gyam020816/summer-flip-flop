ffa4dfb2fe078896d43a976d6ef98a21efe23333
Ha3 <git@ha3.eu>
2019-05-04 19:54:39 +0200

    (evolve) post status back to github

---
+++ _gitlab-ci/post_status_success.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SCRIPT__MIRROR_LOCATION=$(cat .gitlab-to-github-mirror-statuses)
+SCRIPT__STATE=success
+SCRIPT__DESCRIPTION="Pipeline success"
+curl -d "{\"state\":\"$SCRIPT__STATE\", \"context\":\"gitlab-ci\", \"description\":\"$SCRIPT__DESCRIPTION\",\"target_url\":\"$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID\"}" -H 'Content-Type: application/json' -u Hurricaaane:$GITHUB_STATUS_TOKEN https://api.github.com/repos/$SCRIPT__MIRROR_LOCATION/statuses/$CI_BUILD_REF
